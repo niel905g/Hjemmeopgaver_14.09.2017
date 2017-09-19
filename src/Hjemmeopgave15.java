46e6b8b2161ff4d32b1226e44a8262a3b408932b
niel905g <31951130+niel905g@users.noreply.github.com>
2017-09-19 17:04:25 +0200

    Add files via upload

---
+++ src/Hjemmeopgave15.java
@@ -0,0 +1,12 @@
+public class Hjemmeopgave15 {
+    public static void main(String[] args) {
+
+
+        System.out.println("7.5 X 6.5 - 4.5 X 3");
+        System.out.println("-------------------- = ");
+        System.out.println("    47.5 - 5.5");
+
+        System.out.println(((7.5 * 6.5)-(4.5 * 3))/(47.5-5.5));
+
+    }
+}
