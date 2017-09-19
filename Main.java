a99b3271faff646c4a54acdfeb3cdcc380afecfc
niel905g <31951130+niel905g@users.noreply.github.com>
2017-09-19 17:21:46 +0200

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-//Hello World and Welcome to my Java World, with an extra line in the beginning.
-    public static void main(String[] args) {
-        System.out.println("\nHello World!");
-        System.out.println("Welcome to my Java World!");
-    }
-}
