74fc0585f82046a9bf93d48b0646d4923e94cace
niel905g <31951130+niel905g@users.noreply.github.com>
2017-09-19 17:43:00 +0200

    Add files via upload

---
+++ src/Hjemmeopgave16b.java
@@ -0,0 +1,9 @@
+public class Hjemmeopgave16b {
+//Alternativ (nemmere) måde at gøre det på!
+
+        public static void main(String[] args) {
+            int answer = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9;
+
+            System.out.println("1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 = " + answer);
+        }
+    }
